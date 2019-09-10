736262994ad755c35a39921d13c90d3e40989355
autumn-ragland <raglanda998@gmail.com>
2019-09-10 14:49:10 -0500

    Graded 09-10-2019 14:48:37

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
-// 1) Print the first p tag to the console using the ID, class, and tag selectors.
-
+// 1) Print the first p tag to the console using the ID, class, and tag selectors
+.
+// !! : you are only printing once
 // document.getElementsByClassName("special");
-// document.getElementById("first");
-// document.getElementsByTagName("<p>");
+document.getElementById("first");
+document.getElementsByTagName("<p>");
 console.log(document.getElementsByClassName("special")[0]);
-
 
 //2) Print the special class using both the query selector and query all selector.
 
-// document.querySelectorAll("special");
-// console.log(document.querySelectorAll('p.special')[0]);
-
-
+// !! : you are only printing once
+document.querySelectorAll("special");
+console.log(document.querySelectorAll('p.special')[0]);
 
 //3) Change the color of the h1 statement to blue.
 
-// document.getElementsByTagName("<h1>").style.color = "blue";
-
-
-
+// !! : tag name should not be in anchor brackets 
+document.getElementsByTagName("<h1>").style.color = "blue";
 
 //4) Change the color of the last p tag to yellow.
 
-
-
+// !! : incomplete
