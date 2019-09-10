45b0ea47591fbc96e11d17158f9fbde6c6b331fa
flashfan88 <lavarjohnson20@gmail.com>
2019-09-10 12:13:11 -0500

    meh

---
+++ index.js
@@ -0,0 +1,26 @@
+// 1) Print the first p tag to the console using the ID, class, and tag selectors.
+
+// document.getElementsByClassName("special");
+// document.getElementById("first");
+// document.getElementsByTagName("<p>");
+console.log(document.getElementsByClassName("special")[0]);
+
+
+//2) Print the special class using both the query selector and query all selector.
+
+// document.querySelectorAll("special");
+// console.log(document.querySelectorAll('p.special')[0]);
+
+
+
+//3) Change the color of the h1 statement to blue.
+
+// document.getElementsByTagName("<h1>").style.color = "blue";
+
+
+
+
+//4) Change the color of the last p tag to yellow.
+
+
+
